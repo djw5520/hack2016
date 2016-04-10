49f9c66ab061196ff2a56d1d33b42d928eed8719
Daniel Witman <Daniel Witman>
2016-04-10 10:03:42 -0400

    sf

--- SafeStreets3/app/src/main/java/com/example/dwitm/safestreets3/MyActivity.java
+++ SafeStreets3/app/src/main/java/com/example/dwitm/safestreets3/MyActivity.java
@@ -2,18 +2,21 @@ package com.example.dwitm.safestreets3;
 
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
-
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,8 +24,17 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-
 import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
 
 
 public class MyActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
@@ -32,6 +44,17 @@ public class MyActivity extends AppCompatActivity implements GoogleApiClient.OnC
     double latitude;
     double longitude;
     String googleLink;
+    private static final int REQUEST_ENABLE_BT = 5;
+
+
+    public static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+    //Bluetooth
+    private BluetoothDevice mBluetoothDevice;
+    private BluetoothSocket mBluetoothSocket;
+    private BluetoothSocket fallbackSocket;
+    private InputStream mBluetoothInputStream;
+    private Set<BluetoothDevice> pairedDevices;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +71,8 @@ public class MyActivity extends AppCompatActivity implements GoogleApiClient.OnC
         final TextView phone_sub = (TextView) findViewById(R.id.phone_sub);
         final TextView location_sub = (TextView) findViewById(R.id.location_sub);
         final TextView all_clear = (TextView) findViewById(R.id.all_clear);
+
+
 
         assert location != null;
         location.setVisibility(View.GONE);
@@ -69,9 +94,78 @@ public class MyActivity extends AppCompatActivity implements GoogleApiClient.OnC
         all_clear.setVisibility(View.VISIBLE);
 
         assert receive_data != null;
+        final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            System.out.println("NO BLUETOOTH SUPPORT");
+        }
+
+        // Enable Bluetooth adapter
+        assert mBluetoothAdapter != null;
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        pairedDevices = mBluetoothAdapter.getBondedDevices();
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            List<String> pairedDeviceList = new ArrayList<String>();
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in the alert dialog
+                pairedDeviceList.add(device.getName() + "\n" + device.getAddress());
+            }
+
+            // Build an alert dialog to select the Bluetooth adapter from a list of paired devices
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Select the Bluetooth adapter");
+            builder.setItems(pairedDeviceList.toArray(new
+                    CharSequence[pairedDeviceList.size()]), new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Get selected paired Bluetooth device
+                    mBluetoothDevice = (BluetoothDevice) pairedDevices.toArray()[which];
+                    System.out.println(mBluetoothDevice.getAddress());
+                    try {
+                        // Try to connect to Bluetooth device
+                        mBluetoothSocket = mBluetoothDevice.createInsecureRfcommSocketToServiceRecord(MY_UUID);
+                        mBluetoothSocket.connect();
+                        System.out.printf("%d", mBluetoothSocket.TYPE_RFCOMM);
+                        mBluetoothInputStream = mBluetoothSocket.getInputStream();
+                    } catch (IOException e) {
+                        try {
+                            // Try to connect to Bluetooth device again in case of error
+                            Class<?> clazz = mBluetoothSocket.getRemoteDevice().getClass();
+                            Class<?>[] paramTypes = new Class<?>[]{Integer.TYPE};
+
+                            Method m = clazz.getMethod("createRfcommSocket", paramTypes);
+                            Object[] params = new Object[]{Integer.valueOf(1)};
+
+                            fallbackSocket = (BluetoothSocket)
+                                    m.invoke(mBluetoothSocket.getRemoteDevice(), params);
+                            fallbackSocket.connect();
+                            mBluetoothInputStream = fallbackSocket.getInputStream();
+                        } catch (IOException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e1) {
+                            e1.printStackTrace();
+                        }
+                        e.printStackTrace();
+
+                    }
+                }
+            });
+            AlertDialog alert = builder.create();
+            alert.show();
+
+        }
+
         receive_data.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                try {
+                    System.out.println(mBluetoothInputStream.read());
 
+                    if (mBluetoothInputStream.read() == 49) {
                         assert location != null;
                         location.setVisibility(View.VISIBLE);
                         assert phone != null;
@@ -90,6 +184,31 @@ public class MyActivity extends AppCompatActivity implements GoogleApiClient.OnC
                         location_sub.setVisibility(View.VISIBLE);
                         assert all_clear != null;
                         all_clear.setVisibility(View.GONE);
+                    }
+                else
+                    {
+                        assert location != null;
+                        location.setVisibility(View.GONE);
+                        assert phone != null;
+                        phone.setVisibility(View.GONE);
+                        assert uber != null;
+                        uber.setVisibility(View.GONE);
+                        assert helping_hand != null;
+                        helping_hand.setVisibility(View.GONE);
+                        assert let_us != null;
+                        let_us.setVisibility(View.GONE);
+                        assert uber_sub != null;
+                        uber_sub.setVisibility(View.GONE);
+                        assert phone_sub != null;
+                        phone_sub.setVisibility(View.GONE);
+                        assert location_sub != null;
+                        location_sub.setVisibility(View.GONE);
+                        assert all_clear != null;
+                        all_clear.setVisibility(View.VISIBLE);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             }
         });
@@ -103,8 +222,6 @@ public class MyActivity extends AppCompatActivity implements GoogleApiClient.OnC
                     longitude = mLastLocation.getLongitude();
                     try {
                         googleLink = "Hi, would you be able to drive me home? My location is: "+"http://maps.google.com/maps?f=q&q=("+Double.toString(latitude)+","+Double.toString(longitude)+")";
-
-
                         SmsManager smsManager = SmsManager.getDefault();
                         smsManager.sendTextMessage("+17172035062", null,googleLink, null, null);
                         Toast.makeText(getApplicationContext(), "SMS sent", Toast.LENGTH_LONG).show();
